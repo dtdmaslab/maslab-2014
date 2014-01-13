cabd63cd700e67192b35d1ff7ecec3926279e5fe
Charles Gordon <cwgordon7@cwgordon7-ubuntu.(none)>
2014-01-13 00:11:34 -0500

    Improvements to KitBotModel.

--- kitbot-demo/src/kitbot/KitBotModel.java
+++ kitbot-demo/src/kitbot/KitBotModel.java
@@ -4,10 +4,13 @@ import jssc.SerialPort;
 import jssc.SerialPortException;
 
 public class KitBotModel {
+	final static double TURN_THRESH = 10;
+	final static double MIN_POWER = 0.2;
+	final static double POWER_SCALE = 0.3;
+
 	private SerialPort serialPort;
     private byte motorA = 0;
     private byte motorB = 0;
-    private PidController pid;
     private ErrorCalculator ec;
     private int current = 5;
     private int desired = 0;
@@ -22,33 +25,40 @@ public class KitBotModel {
             ec = new ErrorCalculator() {
 				@Override
 				public float getError() {
-					// TODO Auto-generated method stub
-					return current - desired;
+					return normalize(current - desired);
 				}
-            	
             };
-            pid = new PidController(1, 0, 1, ec, new ErrorHandler() {
+            // TODO: Tune these parameters based on experimentation.
+            new PidController(1, 0, 0.75f, ec, new ErrorHandler() {
 				@Override
 				public void handleError(float error) {
 					double m1 = error / 180.0;
 					double m2 = error / -180.0;
 					setMotors(m1 + f, m2 + f);
 				}
-            	
             });
-            System.out.println("????");
-            //drawSquare();
-            forward(0.5, 1000);
+
+            // Go forward.
+            // forward(0.5 /* Speed */, 1500 /* Time in ms */);
+
+            // Turn.
+            // turn(90 /* Degrees clockwise */);
+
+            // Draw a square.
+            // drawSquare();
+
+            // Track closest red ball.
+            // trackRed();
         }
         catch (Exception ex){
             System.out.println(ex);
         }
 	}
-	
+
 	private void drawSquare() {
 		turn(0);
 		for (int i = 0; i < 4; i++) {
-			forward(0.5, 500);
+			forward(0.5, 1500);
 			turn(90);
 		}
 	}
@@ -58,24 +68,28 @@ public class KitBotModel {
 			f = speed;
 			Thread.sleep(millis);
 			f = 0;
-			
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
 
 	private void turn(int degrees) {
+		// TODO: Why is this clockwise? Seems wrong but it works that way.
+		// Could just add a negative sign but we should try to understand why
+		// this is happening.
 		desired += degrees;
 		waitUntilHeadingNormal();
 	}
 
 	private void waitUntilHeadingNormal() {
-		while (Math.abs(current - desired) > 10) { System.out.println(Math.abs(current - desired)); Thread.yield(); }
+		while (Math.abs(normalize(current - desired)) > TURN_THRESH) { System.out.println(Math.abs(current - desired)); Thread.yield(); }
 	}
 
 	public void setMotors( double powerA, double powerB ) {
 		powerA = Math.min(Math.max(-1, powerA), 1);
 		powerB = Math.min(Math.max(-1, powerB), 1);
+		powerA = MIN_POWER * Math.signum(powerA) + POWER_SCALE * powerA;
+		powerB = MIN_POWER * Math.signum(powerB) + POWER_SCALE * powerB;
 		motorA = (byte)(-powerA*127);
 		motorB = (byte)(powerB*127);
 		modified();
